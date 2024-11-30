9109c6cb12730b30ca445314397c57d963797ceb
Abdalrahman Abdalqader <salehabod63@gmail.com>
2024-11-30 15:34:50 +0300

    iteration and recursiv analysis

---
+++ factorial-code-analysis.cpp
@@ -0,0 +1,55 @@
+
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+long long itirative_factorial(int n)
+{
+	long long factorial = 1;
+
+	for (long long i = 1; i <= n; i++)
+	{
+		factorial *= i;
+	}
+	return factorial;
+}
+
+long long recursion_factorial(int n){
+		if(n<=1)
+		return 1;
+		return n*recursion_factorial(n-1);
+	}
+	
+	
+int main()
+{
+
+	int n;
+	cout<<"Enter the value of number : ";
+	cin >> n;
+	if (n > 0)
+	{
+	
+		chrono::time_point<chrono::system_clock> start, end;
+
+		start = chrono::system_clock::now();
+		cout << itirative_factorial(n) << endl;
+		end = chrono::system_clock::now();
+		chrono::duration<double> execution_time = end - start;
+		cout << "execution time: " << execution_time.count() << "s\n";
+
+//    This for recursion factorial :
+
+// 			chrono::time_point<chrono::system_clock> start, end;
+// 		  start = chrono::system_clock::now();
+// 	 cout<< recursion_factorial(n)<<endl;
+//     end = chrono::system_clock::now();
+// 	  chrono::duration<double> execution_time = end - start;
+//    cout  << "execution time: " << execution_time.count() << "s\n";
+	}
+	else
+    {
+        cout << "Please enter a positive number.\n";
+    }
+
+}
